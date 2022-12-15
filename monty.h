6472424a17ee6f12c0e274795158ef9cdb26920c
Emmanuel Makokha <99004575+MamaiTheCoder@users.noreply.github.com>
2022-12-15 12:18:00 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -66,6 +66,7 @@ void get_rotl(stack_t **stack, unsigned int line_number);
 void get_pchar(stack_t **stack, unsigned int line_number);
 void get_rotr(stack_t **stack, unsigned int line_number);
 void get_pstr(stack_t **stack, unsigned int line_number);
+void get_stack(stack_t **stack, int x);
 
 void get_free(stack_t *stack);
 int _isdigit(char *str);
